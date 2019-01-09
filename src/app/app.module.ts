a460a5b4ee25b61b07ac3fbce18d338dd8b5716a
Netter <rafal.netter@aecom.com>
2019-01-09 15:02:18 +0100

    Interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShinyButtonComponent } from './shiny-button/shiny-button.component';
+import { MediaItemComponent } from './media-item/media-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ShinyButtonComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ShinyButtonComponent,
+      MediaItemComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ src/app/media-item/media-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-media-item',
+  templateUrl: './media-item.component.html',
+  styleUrls: ['./media-item.component.css']
+})
+export class MediaItemComponent implements OnInit {
+
+  name = 'The Redemption';
+
+  wasWatched() {
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
