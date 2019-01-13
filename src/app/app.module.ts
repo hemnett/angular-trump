c49bfe649188d3f0c27b9575ce50cf4d5d236422
Raf Net <hemnet77@gmail.com>
2019-01-13 21:01:38 +0100

    Validators build-in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +24,7 @@ import { MediaItemFormComponent } from './media-item-form/media-item-form.compon
    ],
    imports: [
       BrowserModule,
-      FormsModule,
+      ReactiveFormsModule,
       AppRoutingModule
    ],
    providers: [],
--- src/app/media-item-form/media-item-form.component.ts
+++ src/app/media-item-form/media-item-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-media-item-form',
@@ -7,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaItemFormComponent implements OnInit {
 
+  form;
+
   onSubmit(mediaItem) {
     console.log(mediaItem);
   }
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'medium': new FormControl('Movies'),
+      'name': new FormControl('', Validators.compose([
+          Validators.required, Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      'category': new FormControl(''),
+      'year': new FormControl(''),
+    });
   }
 
 }
